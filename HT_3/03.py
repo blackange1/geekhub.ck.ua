77ecc0e5f536bd1ecb92d4bbe9a577d449608b9e
Volodymyr Babenko <gomerbabenko@gmail.com>
2021-11-24 11:43:35 +0200

    update 03.py

--- HT_3/03.py
+++ HT_3/03.py
@@ -1,14 +1,16 @@
 # 3. Написати функцiю season, яка приймає один аргумент — номер мiсяця (вiд 1 до 12), яка буде повертати пору року, якiй цей мiсяць належить (зима, весна, лiто або осiнь)
 
-months = {
-    (12, 1, 2): 'зима',
-    (3, 4, 5): 'весна',
-    (6, 7, 8): 'лiто',
-    (9, 10, 11): 'осiнь',
-}
+def season(n):
+    months = {
+        (12, 1, 2): 'зима',
+        (3, 4, 5): 'весна',
+        (6, 7, 8): 'лiто',
+        (9, 10, 11): 'осiнь',
+    }
+    for key, month in months.items():
+        if n in key:
+            return month
+
 
 n = int(input('enter number month (1 - 12)\n'))
-for key, month in months.items():
-    if n in key:
-        print(month)
-        break
+print(season(n))
